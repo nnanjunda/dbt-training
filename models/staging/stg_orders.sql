b3203f7ed442c09295964b485b0dc07fe53dc04b
nnanjunda@deloitte.com.au <nnanjunda@deloitte.com.au>
2022-08-16 21:09:30 +0000

    developed report profit by customer and profit by product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -7,7 +7,7 @@ o.ordersellingprice - o.ordercostprice as orderProfit,
 o.ordercostprice,
 o.ordersellingprice,
 c.customerid, c.segment, c.country,
-p.category,p.productname,p.subcategory
+p.category,p.productname,p.subcategory,p.productid
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.customerid=c.customerid
