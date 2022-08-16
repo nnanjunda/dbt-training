1a1194156ae6c068ea9b588f0f0eca0239dc0931
nnanjunda@deloitte.com.au <nnanjunda@deloitte.com.au>
2022-08-16 20:59:02 +0000

    Developing Stage order

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,15 @@
+select 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderProfit,
+o.ordercostprice,
+o.ordersellingprice,
+c.customerid, c.segment, c.country,
+p.category,p.productname,p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid=c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid=p.productid
